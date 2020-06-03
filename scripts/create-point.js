ce221c41acdb7941d8ab930e73ef57b90668707b
gabriel.soares02 <gabriel.soares02@latam.com>
2020-06-03 15:25:48 -0300

    add page search-results

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,8 +2,13 @@ const UFSelect = document.querySelector("[name=uf]");
 const citySelect = document.querySelector("[name=city]");
 const stateInput = document.querySelector("[name=state]");
 
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+const collectedItems = document.querySelector("[name=items]");
+let selectedItems = [];
+
 function clearCitySelect() {
   citySelect.innerHTML = `<option value="">Selecione a cidade</option>`;
+  citySelect.disabled = true;
 }
 
 function selectAPI(url, select) {
@@ -11,7 +16,10 @@ function selectAPI(url, select) {
     .then((res) => res.json())
     .then((items) => {
       for (const item of items) {
-        select.innerHTML += `<option value='${item.id}'>${item.nome}</option>`;
+        if (select === UFSelect)
+          select.innerHTML += `<option value='${item.id}'>${item.nome}</option>`;
+        if (select === citySelect)
+          select.innerHTML += `<option value='${item.nome}'>${item.nome}</option>`;
       }
     });
 }
@@ -36,7 +44,33 @@ function populateCities(event) {
   citySelect.disabled = false;
 }
 
+function setCollectedItems(itemLi) {
+  const itemId = itemLi.dataset.id;
+  const alreadySelected = selectedItems.findIndex((item) => item == itemId);
+
+  if (alreadySelected >= 0) {
+    const filteredItems = selectedItems.filter((item) => item != itemId);
+    selectedItems = filteredItems;
+  } else {
+    selectedItems.push(itemId);
+  }
+
+  collectedItems.value = selectedItems;
+}
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  itemLi.classList.toggle("selected");
+
+  setCollectedItems(itemLi);
+}
+
 populateUFs();
 clearCitySelect();
 
 UFSelect.addEventListener("change", populateCities);
+
+for (let item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
