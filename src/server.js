7ceec2137e0803fd283f3e64992b7617b0ba2a86
gabriel.soares02 <gabriel.soares02@latam.com>
2020-06-04 12:20:05 -0300

    feat: config server

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const nunjucks = require("nunjucks");
+const server = express();
+
+server.use(express.static("public"));
+
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
+server.get("/", (req, res) =>
+  res.render("index.html", { title: "Seu marketplace de coleta de resíduos" })
+);
+server.get("/create-point", (req, res) => res.render("create-point.html"));
+server.get("/search-results", (req, res) => res.render("search-results.html"));
+
+server.listen(3000);
